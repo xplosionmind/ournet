38fa4a30950a46b03fd2d905c325a87f8a3fc76c
Tommi <surfing@tommi.space>
2024-09-13 20:50:17 +0300

    🏗️ Improve Eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,40 @@
+import {
+	EleventyRenderPlugin,
+	InputPathToUrlTransformPlugin,
+	IdAttributePlugin,
+} from '@11ty/eleventy';
 import yaml from 'js-yaml';
+
+import markdownIt from 'markdown-it';
+let mdIt = markdownIt({
+	html: true,
+	breaks: true,
+	linkify: true,
+});
+
 export default function (eleventyConfig) {
-	eleventyConfig.addPassthroughCopy({'./assets/':'/'});
+	eleventyConfig.setLibrary('md', mdIt);
 	eleventyConfig.addDataExtension('yaml,yml', contents => yaml.load(contents));
+
+	eleventyConfig.addPassthroughCopy({ './assets/': '/' });
 	eleventyConfig.addPassthroughCopy('./ournet/style.css');
 	eleventyConfig.addWatchTarget('./ournet/style.css');
+
+	eleventyConfig.addFilter('markdownify', function (content) {
+		return mdIt.renderInline(content);
+	});
+
+	eleventyConfig.addPlugin(EleventyRenderPlugin);
+	eleventyConfig.addPlugin(InputPathToUrlTransformPlugin, {
+		extensions: 'html,liquid,md',
+	});
+	eleventyConfig.addPlugin(IdAttributePlugin);
+
 	return {
 		dir: {
 			input: 'ournet',
 			output: 'www',
-			layouts: '_layouts'
-		}
+			layouts: '_layouts',
+		},
 	};
 }

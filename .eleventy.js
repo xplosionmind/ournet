22b9a72811e8ee3411a92acad09c0b1806e43899
Tommi <surfing@tommi.space>
2024-09-13 20:49:19 +0300

    🏗️ Add YAML support for data files

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
+import yaml from 'js-yaml';
 export default function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy({'./assets/':'/'});
+	eleventyConfig.addDataExtension('yaml,yml', contents => yaml.load(contents));
 	eleventyConfig.addPassthroughCopy('./ournet/style.css');
 	eleventyConfig.addWatchTarget('./ournet/style.css');
 	return {

c1a94fa7ad0a9ffb8d2741b3d18bc6a0bb66fa40
Tommi <surfing@tommi.space>
2025-01-16 09:55:49 +0100

    🔧 Various minor fixes

--- .eleventy.js
+++ .eleventy.js
@@ -1,39 +1,33 @@
-import {
-	EleventyRenderPlugin,
-	InputPathToUrlTransformPlugin,
-	IdAttributePlugin,
-} from '@11ty/eleventy';
-import { eleventyImageTransformPlugin } from '@11ty/eleventy-img';
-import yaml from 'js-yaml';
+import { EleventyRenderPlugin, InputPathToUrlTransformPlugin, IdAttributePlugin } from 'npm:@11ty/eleventy';
+import { eleventyImageTransformPlugin } from 'npm:@11ty/eleventy-img';
+import markdownIt from 'npm:markdown-it';
+import miniHtml from 'npm:html-minifier-terser';
+import yaml from 'npm:js-yaml';
 import { parse as csvParse } from 'csv-parse/sync';
-import miniHtml from 'html-minifier-terser';
 
-import markdownIt from 'markdown-it';
-let mdIt = markdownIt({
+const md = markdownIt({
 	html: true,
-	breaks: true,
 	linkify: true,
+	typographer: true
 });
 
 export default function (eleventyConfig) {
-	eleventyConfig.setLibrary('md', mdIt);
+
+	eleventyConfig.setDataFileBaseName('root');
+	eleventyConfig.addPassthroughCopy({ './ournet/assets/': '/' });
+	eleventyConfig.addPassthroughCopy('./ournet/style.css');
+	eleventyConfig.addPassthroughCopy('./ournet/print.css');
+	eleventyConfig.addWatchTarget('./ournet/*.css');
+	eleventyConfig.setLibrary('md', md);
+
+	// Data files //
 	eleventyConfig.addDataExtension('yaml,yml', contents => yaml.load(contents));
 	eleventyConfig.addDataExtension('csv', contents => csvParse(contents, {
 		columns: true,
 		skip_empty_lines: true
 	}));
 
-	eleventyConfig.setDataFileBaseName('root');
-
-	eleventyConfig.addPassthroughCopy({ './ournet/assets/': '/' });
-	eleventyConfig.addPassthroughCopy('./ournet/style.css');
-	eleventyConfig.addPassthroughCopy('./ournet/print.css');
-	eleventyConfig.addWatchTarget('./ournet/*.css');
-
-	eleventyConfig.addFilter('markdownify', function (content) {
-		return mdIt.renderInline(content);
-	});
-
+	// Plugins //
 	eleventyConfig.addPlugin(EleventyRenderPlugin);
 	eleventyConfig.addPlugin(InputPathToUrlTransformPlugin, {
 		extensions: 'html,liquid,md',
@@ -44,23 +38,43 @@ export default function (eleventyConfig) {
 		formats: ['webp', 'auto'],
 		defaultAttributes: {
 			loading: 'lazy',
-			decoding: 'async'
+			decoding: 'async',
+			sizes: 'auto'
 		},
 		outputDir: './www/img/'
 	});
 
+	// Filters //
+	eleventyConfig.addFilter('markdownify', function (content) {
+		return md.renderInline(content);
+	});
+	eleventyConfig.addFilter('obfuscateEmail', (email) => {
+		return email.split('').map(function (char) {
+			return `&#${char.charCodeAt(0)};`;
+		}).join('');
+	});
+
+	// Production-only //
 	if (process.env.ELEVENTY_ENV != 'development') {
 		// Minify output //
-		eleventyConfig.addTransform(miniHtml, function (content) {
+		eleventyConfig.addTransform(miniHtml, async function (content) {
 			if ((this.page.outputPath || '').endsWith('.html')) {
-				let minified = miniHtml.minify(content, {
+				const minified = miniHtml.minify(content, {
+					collapseBooleanAttributes: true,
 					collapseWhitespace: true,
 					decodeEntities: true,
 					minifyCSS: true,
 					minifyJS: true,
-					minifyURLs: true,
+					noNewlinesBeforeTagClose: true,
+					quoteCharacter: "'",
+					removeAttributeQuotes: true,
 					removeComments: true,
 					removeEmptyAttributes: true,
+					removeEmptyElements: true,
+					removeOptionalTags: true,
+					removeRedundantAttributes: true,
+					sortAttributes: true,
+					sortClassName: true,
 					useShortDoctype: true,
 				});
 				return minified;
